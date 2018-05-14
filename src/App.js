f043ea7ffe1a1e062489e7ffbedc5c5ad0f22bbc
mohamad chorakchi <m.chorakchi@ymail.com>
2018-05-14 23:25:48 +0430

    add Appbar with BTN App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppBar from './component/impolement/AppBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <AppBar/>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
